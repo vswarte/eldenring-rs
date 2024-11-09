943c86c9eebdcc0008dd34640587effbab562d8e
Vincent Swarte <vincent@vinsert.nl>
2024-11-09 20:33:36 +0100

    Make cam_override_chr_ins nullable on WorldChrManDbg

--- crates/game/src/cs/world_chr_man_dbg.rs
+++ crates/game/src/cs/world_chr_man_dbg.rs
@@ -6,7 +6,7 @@ pub struct WorldChrManDbg<'a> {
     pub unk0: [u8; 0xa8],
     pub manipulator: usize,
     pub player_session_holder: usize,
-    pub cam_override_chr_ins: &'a mut ChrIns<'a>,
+    pub cam_override_chr_ins: Option<&'a mut ChrIns<'a>>,
 }
 
 impl DLRFLocatable for WorldChrManDbg<'_> {
