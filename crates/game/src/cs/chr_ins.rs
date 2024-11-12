feb168709a5cd36600bb7af4983cab4fbb642448
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2024-11-12 13:30:51 +0300

    changes by request

--- crates/game/src/cs/chr_ins.rs
+++ crates/game/src/cs/chr_ins.rs
@@ -295,11 +295,11 @@ pub struct CSChrEventModule {
     pub event_animation: i32,
     pub last_animation: i32,
     pub init_stay_id: i32,
-    unk24: [u8; 0x4],
+    unk24: u32,
     pub ez_state_request_ladder: i32,
     unk2c: [u8; 0xB],
     pub msg_map_list_call: i32,
-    unk3c: [u8; 0x4],
+    unk3c: u32,
     pub flags: u8, // bit in pos 1 is iframes
     unk41: [u8; 0xA],
     pub ez_state_request_ladder_output: i32,
