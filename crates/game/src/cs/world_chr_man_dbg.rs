a477828014abcf9084890e3cb21f3c7b07b05910
Vincent Swarte <vincent@vinsert.nl>
2024-11-12 11:21:02 +0100

    day drinking ftw

--- crates/game/src/cs/world_chr_man_dbg.rs
+++ crates/game/src/cs/world_chr_man_dbg.rs
@@ -5,7 +5,7 @@ use crate::DLRFLocatable;
 #[repr(C)]
 pub struct WorldChrManDbg {
     pub unk0: [u8; 0xa8],
-    pub manipulator: usize,
+    pub debug_manipulator: usize,
     pub player_session_holder: usize,
     pub cam_override_chr_ins: Option<OwningPtr<ChrIns>>,
 }
