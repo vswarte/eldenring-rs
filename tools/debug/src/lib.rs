d14c0fe434cb37ba761f9da640aa814495052f49
Vincent Swarte <vincent@vinsert.nl>
2025-03-31 06:31:39 +0200

    Fix debug dll for clippy

--- tools/debug/src/lib.rs
+++ tools/debug/src/lib.rs
@@ -33,6 +33,8 @@ use util::system::wait_for_system_init;
 mod display;
 mod rva;
 
+/// # Safety
+/// This is exposed this way such that libraryloader can call it. Do not call this yourself.
 #[no_mangle]
 pub unsafe extern "C" fn DllMain(hmodule: HINSTANCE, reason: u32) -> bool {
     if reason == 1 {
