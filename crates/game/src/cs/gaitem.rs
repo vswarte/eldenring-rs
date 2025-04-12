be47f50a89c3d581994586cb039241135efb5c32
Vincent Swarte <vincent@vinsert.nl>
2025-04-12 16:19:55 +0200

    Rework some From impls to const ones, remove Display impl from CSGaitemHandle and ItemId

--- crates/game/src/cs/gaitem.rs
+++ crates/game/src/cs/gaitem.rs
@@ -3,28 +3,18 @@ use std::fmt::Display;
 use crate::pointer::OwnedPtr;
 
 use super::ItemId;
-
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-pub enum GaitemCategory {
-    Weapon = 0,
-    Protector = 1,
-    Accessory = 2,
-    Goods = 3,
-    Gem = 4,
-}
-
-impl GaitemCategory {
-    pub const fn from_u8(val: &u8) -> Result<Self, ()> {
-        Ok(match val {
-            0 => GaitemCategory::Weapon,
-            1 => GaitemCategory::Protector,
-            2 => GaitemCategory::Accessory,
-            3 => GaitemCategory::Goods,
-            4 => GaitemCategory::Gem,
-            _ => return Err(()),
-        })
-    }
+#[repr(C)]
+pub struct CSGaitemImp {
+    vftable: usize,
+    pub gaitem_instances: [OwnedPtr<CSGaitemIns>; 5120],
+    pub gaitem_entries: [CSGaitemImpEntry; 5120],
+    pub indexes: [u32; 5120],
+    pub write_index: u32,
+    pub read_index: u32,
+    rand_xorshift: [u8; 0x18],
+    unk23028: [u8; 8],
+    pub is_being_serialized: bool,
+    unk23038: [u8; 7],
 }
 
 #[repr(C)]
@@ -38,20 +28,6 @@ pub struct CSGaitemIns {
 pub struct CSGaitemImpEntry {
     pub unindexed_gaitem_handle: u32,
     pub ref_count: u32,
-}
-
-#[repr(C)]
-pub struct CSGaitemImp {
-    vftable: usize,
-    pub gaitem_instances: [OwnedPtr<CSGaitemIns>; 5120],
-    pub gaitem_entries: [CSGaitemImpEntry; 5120],
-    pub indexes: [u32; 5120],
-    pub write_index: u32,
-    pub read_index: u32,
-    rand_xorshift: [u8; 0x18],
-    unk23028: [u8; 8],
-    pub is_being_serialized: bool,
-    unk23038: [u8; 7],
 }
 
 #[repr(C)]
@@ -82,15 +58,36 @@ impl GaitemHandle {
     }
 }
 
-impl Display for GaitemHandle {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "Gaitem Handle: {:0>8}, Category: {:?}, Selector: {:0>6}, Indexed: {:?}",
-            self.0,
-            self.category(),
-            self.selector(),
-            self.is_indexed()
-        )
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum GaitemCategory {
+    Weapon = 0,
+    Protector = 1,
+    Accessory = 2,
+    Goods = 3,
+    Gem = 4,
+}
+
+impl GaitemCategory {
+    pub const fn from_u8(val: &u8) -> Result<Self, ()> {
+        Ok(match val {
+            0 => GaitemCategory::Weapon,
+            1 => GaitemCategory::Protector,
+            2 => GaitemCategory::Accessory,
+            3 => GaitemCategory::Goods,
+            4 => GaitemCategory::Gem,
+            _ => return Err(()),
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::cs::{CSGaitemImp, CSGaitemIns};
+
+    #[test]
+    fn proper_sizes() {
+        assert_eq!(0x19038, size_of::<CSGaitemImp>());
+        assert_eq!(0x10, size_of::<CSGaitemIns>());
     }
 }
--- crates/game/src/cs/item_id.rs
+++ crates/game/src/cs/item_id.rs
@@ -15,18 +15,17 @@ pub enum ItemCategory {
     None = -1,
 }
 
-impl From<i8> for ItemCategory {
-    fn from(value: i8) -> Self {
-        match value {
+impl ItemCategory {
+    pub const fn from_i8(value: i8) -> Result<Self, ()> {
+        Ok(match value {
             0 => ItemCategory::Weapon,
             1 => ItemCategory::Protector,
             2 => ItemCategory::Accessory,
             4 => ItemCategory::Goods,
             8 => ItemCategory::Gem,
-            // 15 = u4::MAX
             15 | -1 => ItemCategory::None,
-            _ => panic!("Invalid item category: {}", value),
-        }
+            _ => return Err(()),
+        })
     }
 }
 
@@ -42,28 +41,13 @@ impl ItemId {
         self.0 & 0x0FFFFFFF
     }
 
-    pub fn category(&self) -> ItemCategory {
-        ItemCategory::from((self.0 >> 28) as i8)
+    pub const fn category(&self) -> Result<ItemCategory, ()> {
+        ItemCategory::from_i8((self.0 >> 28) as i8)
     }
 }
 
 impl From<i32> for ItemId {
     fn from(value: i32) -> Self {
         Self(value)
-    }
-}
-
-impl Display for ItemId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.category() == ItemCategory::None {
-            write!(f, "Item ID: -1")
-        } else {
-            write!(
-                f,
-                "Item ID: {}, Category: {:?}",
-                self.item_id(),
-                self.category()
-            )
-        }
     }
 }
