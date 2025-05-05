690817e2514a4891f28ec2507653050f3811ae23
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2025-05-05 19:04:30 +0300

    Update `ChrIns` `last_used_item` type

--- crates/eldenring/src/cs/chr_ins.rs
+++ crates/eldenring/src/cs/chr_ins.rs
@@ -20,6 +20,7 @@ use crate::cs::session_manager::{SessionManagerPlayerEntry, SessionManagerPlayer
 use crate::cs::task::{CSEzRabbitNoUpdateTask, CSEzVoidTask};
 use crate::cs::world_chr_man::{ChrSetEntry, WorldBlockChr};
 use crate::cs::world_geom_man::{CSMsbParts, CSMsbPartsEne};
+use crate::cs::ItemId;
 
 use super::{ItemId, NpcSpEffectEquipCtrl, SpecialEffect};
 
@@ -128,10 +129,10 @@ pub struct ChrIns {
     pub target_velocity_recorder: usize,
     unkc8: usize,
     pub lock_on_target_position: FSVector4,
-    unke0: [u8; 0x80],
-    tae_unk_use_item: ItemId,
+    unkd8: [u8; 0x80],
     /// Used by TAE's UseGoods to figure out what item to actually apply.
     pub tae_queued_use_item: ItemId,
+    unk164: u32,
     unk168: u32,
     unk16c: u32,
     unk170: u32,
