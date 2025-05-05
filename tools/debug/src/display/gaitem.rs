c2a25f28dc2bf27f35c2dad9a4cd67fb8e5f636f
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2025-05-05 19:08:29 +0300

    Update `CSGaitemImp` debug view

--- tools/debug/src/display/gaitem.rs
+++ tools/debug/src/display/gaitem.rs
@@ -1,47 +1,56 @@
 use eldenring::cs::CSGaitemImp;
-use hudhook::imgui::{TableColumnSetup, TreeNodeFlags};
+use hudhook::imgui::{TableColumnSetup, TableFlags, TreeNodeFlags};
 
 use super::DebugDisplay;
 
 impl DebugDisplay for CSGaitemImp {
     fn render_debug(&self, ui: &&mut hudhook::imgui::Ui) {
         if ui.collapsing_header("Gaitem Inses", TreeNodeFlags::empty()) {
-            if let Some(_t) = ui.begin_table_header(
+            ui.indent();
+            if let Some(_t) = ui.begin_table_header_with_flags(
                 "cs-gaitem-imp-gaiteminses",
                 [
+                    TableColumnSetup::new("Index"),
                     TableColumnSetup::new("Handle"),
                     TableColumnSetup::new("Item ID"),
                     TableColumnSetup::new("Category"),
                     TableColumnSetup::new("Additional"),
                 ],
+                TableFlags::RESIZABLE
+                    | TableFlags::BORDERS
+                    | TableFlags::ROW_BG
+                    | TableFlags::SIZING_STRETCH_PROP,
             ) {
-                ui.indent();
                 for gaitem in self.gaitems.iter().filter_map(|f| f.as_ref()) {
                     let gaitem = gaitem.as_ref();
+                    let index = gaitem.gaitem_handle.index() as i32;
 
                     ui.table_next_column();
-                    ui.text(format!("{:x?}", gaitem.gaitem_handle));
+                    ui.text(format!("{index:?}"));
 
                     ui.table_next_column();
-                    ui.text(format!("{:?}", gaitem.item_id));
+                    ui.text(gaitem.gaitem_handle.to_string());
+
+                    ui.table_next_column();
+                    ui.text(gaitem.item_id.to_string());
 
                     ui.table_next_column();
                     ui.text(format!("{:?}", gaitem.gaitem_handle.category()));
 
                     ui.table_next_column();
                     if let Some(wep) = gaitem.as_wep() {
-                        ui.text(format!(
-                            "AoW handle: {:x?}",
-                            wep.gem_slot_table.gem_slots[0].gaitem_handle
-                        ));
+                        let gem_handle = wep.gem_slot_table.gem_slots[0].gaitem_handle;
+                        if gem_handle.0 != 0 {
+                            ui.text(format!("Gem: {:?}", gem_handle.index()))
+                        }
                     } else if let Some(gem) = gaitem.as_gem() {
-                        ui.text(format!("Gem item ID: {:?}", gem.item_id));
-                    } else {
-                        ui.text("");
+                        if gem.weapon_handle.0 != 0 {
+                            ui.text(format!("Weapon: {:?}", gem.weapon_handle.index()))
+                        }
                     }
                 }
-                ui.unindent();
             }
+            ui.unindent();
         }
     }
 }
